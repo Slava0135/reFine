493dbd918e9bcae12cb4a27fc182ea9d81e15703
slava0135 <super.novalskiy_0135@inbox.ru>
2021-02-07 21:07:55 +0300

    h

--- src/content/ReBlocks.java
+++ src/content/ReBlocks.java
@@ -1,19 +1,29 @@
 package content;
 
 import mindustry.ctype.ContentList;
+import mindustry.type.Category;
 import mindustry.world.Block;
 import mindustry.world.blocks.environment.OreBlock;
+import world.blocks.production.Furnace;
+
+import static mindustry.type.ItemStack.with;
 
 public class ReBlocks implements ContentList {
 
-    public static Block furnace, electricFurnace, granite;
+    public static Block furnace, electricFurnace, oreGranite;
 
     @Override
     public void load() {
-        granite = new OreBlock(ReItems.granite){{
+
+        oreGranite = new OreBlock(ReItems.granite){{
             oreDefault = true;
             oreThreshold = 0.81f;
             oreScale = 23.47619f;
+        }};
+
+        furnace = new Furnace("furnace"){{
+           requirements(Category.production, with(ReItems.granite, 12));
+           size = 2;
         }};
     }
 }
--- src/world/blocks/production/Furnace.kt
+++ src/world/blocks/production/Furnace.kt
@@ -1,11 +1,18 @@
 package world.blocks.production
 
+import mindustry.gen.Sounds
 import mindustry.world.Block
 
-class Furnace(name: String) : Block(name) {
+open class Furnace(name: String) : Block(name) {
 
     override fun init() {
         super.init()
+        update = true
+        solid = true
+        hasItems = true
+        sync = true
+        ambientSound = Sounds.smelter
+        ambientSoundVolume = 0.07f
     }
 
 }
