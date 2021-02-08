9a19883f0b5315acbc0e1924bb6692d29bd8a5de
slava0135 <super.novalskiy_0135@inbox.ru>
2021-02-08 11:18:59 +0300

    h

--- src/refine/content/ReBlocks.kt
+++ src/refine/content/ReBlocks.kt
@@ -13,7 +13,7 @@ class ReBlocks : ContentList {
         oreGranite = object : OreBlock(ReItems.granite) {
             init {
                 oreDefault = true
-                oreThreshold = 0.882f
+                oreThreshold = 0.864f
                 oreScale = 25.380953f
             }
         }
